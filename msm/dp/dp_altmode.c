2a618019df3e9e18706841e7fa4efeb6542676d4
Sankeerth Billakanti <sbillaka@codeaurora.org>
2020-09-23 12:53:33 +0530

    disp: msm: dp: inform multi-func support to usb driver
    The usb controller driver does not know if DP is
    supported / connected and whether it is operating in
    concurrent mode. So, whenever there is a ucsi role switch
    call to the usb controller driver, it will reset the usb
    phy irrespective of DP connection.
    
    The phy reset while DP driver is still processing cable
    connect or disconnect will cause aux timeout or NOC errors.
    This change will inform the usb controller driver about the
    multi_func mode support to convey how many lanes DP will
    be using.
    
    Change-Id: I9e96ed399e879820ddd238cf0fbed7069f1de60d
    Signed-off-by: Sankeerth Billakanti <sbillaka@codeaurora.org>

--- msm/dp/dp_altmode.c
+++ msm/dp/dp_altmode.c
@@ -41,7 +41,12 @@ enum dp_altmode_pin_assignment {
 	DPAM_HPD_F,
 };
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 10, 0)
 static int dp_altmode_set_usb_dp_mode(struct dp_altmode_private *altmode)
+#else
+static int dp_altmode_release_ss_lanes(struct dp_altmode_private *altmode,
+		bool multi_func)
+#endif
 {
 	int rc;
 	struct device_node *np;
@@ -70,7 +75,11 @@ static int dp_altmode_set_usb_dp_mode(struct dp_altmode_private *altmode)
 	}
 
 	while (timeout) {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 10, 0)
 		rc = dwc3_msm_set_dp_mode(&usb_pdev->dev, altmode->connected, altmode->lanes);
+#else
+		rc = dwc3_msm_release_ss_lane(&usb_pdev->dev, multi_func);
+#endif
 		if (rc != -EBUSY)
 			break;
 
@@ -188,7 +197,12 @@ static int dp_altmode_notify(void *priv, void *data, size_t len)
 
 		altmode->dp_altmode.base.orientation = orientation;
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 10, 0)
 		rc = dp_altmode_set_usb_dp_mode(altmode);
+#else
+		rc = dp_altmode_release_ss_lanes(altmode,
+				altmode->dp_altmode.base.multi_func);
+#endif
 		if (rc)
 			goto ack;
 
