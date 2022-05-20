13ecced00eace0fdbbe8398e91759bfef68e0ca0
ruoyyang <quic_ruoyyang@quicinc.com>
2022-05-20 17:06:11 +0800

    disp: msm: sde: Add back allow sde components creation from external module
    Those changes are deleted by 3577010.
    Add it back for shd.
    
    Change-Id: Id0dae000dca4c6e481a3a554e32c982af0eac502
    Signed-off-by: ruoyyang <quic_ruoyyang@quicinc.com>

--- msm/sde/sde_rm.h
+++ msm/sde/sde_rm.h
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2016-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #ifndef __SDE_RM_H__
@@ -404,6 +405,18 @@ static inline int sde_rm_topology_get_num_lm(struct sde_rm *rm,
 bool sde_rm_topology_is_group(struct sde_rm *rm,
 		struct drm_crtc_state *state,
 		enum sde_rm_topology_group group);
+
+/**
+ * sde_rm_ext_blk_create_reserve - Create external HW blocks
+ *	in resource manager and reserve for specific encoder.
+ * @rm: SDE Resource Manager handle
+ * @hw: external HW block
+ * @drm_enc: DRM Encoder handle
+ * @Return: 0 on Success otherwise -ERROR
+ */
+int sde_rm_ext_blk_create_reserve(struct sde_rm *rm,
+				struct sde_hw_blk *hw,
+				struct drm_encoder *enc);
 
 /**
  * sde_rm_ext_blk_destroy - Given the encoder for the display chain, release
