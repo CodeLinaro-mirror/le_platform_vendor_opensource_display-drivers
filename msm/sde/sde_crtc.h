29e61132a571528ad98e66a7deea81e573f5317d
Jonathan McIntosh <jmcintos@qti.qualcomm.com>
2026-05-13 15:10:32 -0700

    disp: msm: cleanup frame data buff mem manipulation
    This change cleans up OOB write and UAF issues within frame data
    buffer logic and ensures there are no race conditions during buffer
    management operations.
    
    Change-Id: Id656ac2ecfa67e68a886c993ee05708ae3d7e4d2
    Signed-off-by: Jonathan McIntosh <jmcintos@qti.qualcomm.com>

--- msm/sde/sde_crtc.h
+++ msm/sde/sde_crtc.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  * Copyright (c) 2015-2021 The Linux Foundation. All rights reserved.
  * Copyright (C) 2013 Red Hat
  * Author: Rob Clark <robdclark@gmail.com>
@@ -369,6 +369,7 @@ enum sde_crtc_hw_fence_flags {
  * @skip_blend_plane_h: skip blend plane height
  * @line_time_in_ns : current mode line time in nano sec is needed for QOS update
  * @frame_data      : Framedata data structure
+ * @frame_data_lock : spinlock to protect framedata allocation, free and access
  * @previous_opr_value : store previous opr values
  * @opr_event_notify_enabled : Flag to indicate if opr event notify is enabled or not
  * @hwfence_features_mask : u32 mask to enable/disable hw fence features. See enum
@@ -488,6 +489,7 @@ struct sde_crtc {
 	u32 line_time_in_ns;
 
 	struct sde_frame_data frame_data;
+	spinlock_t frame_data_lock;
 
 	struct sde_opr_value previous_opr_value;
 	bool opr_event_notify_enabled;
